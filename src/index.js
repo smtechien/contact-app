9a7faad9e4c5cdf4b6b33716afad7b1612eb2556
smtekien <sulthonmutaqin@gmail.com>
2022-06-02 17:51:53 +0800

    first react app - learn from dicoding

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ContactApp from './components/ContactApp';
+import { createRoot } from 'react-dom/client';
+
+// styling
+
+import './styles/style.css';
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(<ContactApp />);
